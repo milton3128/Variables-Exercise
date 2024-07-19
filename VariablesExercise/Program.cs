e6ad4db85e4272675fcd666a232780d86d7bec0c
milton3128 <mannmilton6@gmail.com>
2024-07-18 22:14:27 -0400

    Created Variables and initialized them

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+
+            String dogName = "Jackson";
+
+            int dogAge = 115;
+
+            char firstInitials = 'J';
+
+            bool isHungry = true;
+
+            double dogWeight = 25.23;
+
+            decimal dogHeight = 12.69m;
+
+            Console.WriteLine($"Hello, my dog's name is {dogName} and he is {dogAge} years old.");
+            Console.WriteLine($" {dogName}'s initials is {firstInitials} and it is {isHungry.ToString().ToLower()} he is always hungry.");
+            Console.WriteLine($" {dogName} weighs about {dogWeight} and stands at about {dogHeight} inches.");
         }
     }
 }
